e6af2e6352828dd592c9ee99354e2f89d694dbad
Mark Holmes <smlmrkhlms@gmail.com>
2018-05-27 14:39:22 -0400

    Adds ESLint

--- src/pages/tags/index.js
+++ src/pages/tags/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { kebabCase } from 'lodash';
 import Helmet from 'react-helmet';
 import Link from 'gatsby-link';
+import React from 'react';
+import { kebabCase } from 'lodash';
 
 const TagsPage = ({
   data: {
